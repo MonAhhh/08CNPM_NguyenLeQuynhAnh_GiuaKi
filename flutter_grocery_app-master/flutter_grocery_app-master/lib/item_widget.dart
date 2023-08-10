14a43155e356a181e08b53053efa8bcc101c2cc1
MonAhhh <110552364+MonAhhh@users.noreply.github.com>
2023-08-11 00:47:44 +0700

    first commit

---
+++ flutter_grocery_app-master/flutter_grocery_app-master/lib/item_widget.dart
@@ -0,0 +1,154 @@
+// ignore: unnecessary_import
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, duplicate_ignore
+
+// ignore: unnecessary_import
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+// ignore: unused_import
+import 'package:flutter_grocery_ui/data.dart';
+import 'package:flutter_grocery_ui/product_detail.dart';
+
+import 'models/product.dart';
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return ProductDetail(
+            product: product,
+          );
+        }));
+      },
+      child: Card(
+        elevation: 2,
+        shadowColor: Colors.green,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+              tag: product.images[0],
+              child: SizedBox(
+                width: 150,
+                child: Image.network(product.images[0]),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              'USD ${product.price}',
+              style: const TextStyle(
+                color: Colors.green,
+                fontSize: 12,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                product.title,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            // const SizedBox(
+            //   height: 2,
+            // ),
+            // Text(
+            //   product.price.toString(),
+            //   style: const TextStyle(
+            //     fontSize: 18,
+            //     fontWeight: FontWeight.w300,
+            //   ),
+            // ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Divider(
+              height: 2,
+              color: Colors.grey,
+            ),
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                // ignore: prefer_const_constructors
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // ignore: duplicate_ignore, duplicate_ignore
+                  children: <Widget>[
+                    // ignore: prefer_const_constructors
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.add_shopping_cart,
+                          size: 20,
+                          color: Colors.green,
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        Text(
+                          "Beli",
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.remove_circle_outline,
+                          size: 18,
+                          color: Colors.green,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Text('0'),
+                        ),
+                        Icon(
+                          Icons.add_circle_outline,
+                          size: 18,
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_login_fashion-master/flutter_login_fashion-master/README.md
@@ -0,0 +1,21 @@
+# Flutter Login Fashion UI
+
+Layouting the UI for Login Fashion
+
+## Video Youtube
+
+[![Watch the video](https://img.youtube.com/vi/iL6qPggKKJ8/sddefault.jpg)](https://youtu.be/iL6qPggKKJ8)
+
+https://youtu.be/iL6qPggKKJ8
+
+
+
+
+## ScreenShot
+
+| Login        |
+|--------------|
+| <img src="img1.png" width="500"/>
+
+
+
